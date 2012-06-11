98733d43ebee0b630359cc1d3adb8e8a532c1eca
Joseph Salisbury <salisbury.joseph@gmail.com>
2012-06-11 18:19:24 +0100

    Add: Comments for decode function, integer decoding section.

--- bencode.py
+++ bencode.py
@@ -13,12 +13,16 @@ class DecodeError(Exception):
 # regular expression to match a bencoded integer.
 BENCODED_INTEGER_RE = re.compile("i-*[0-9]+e")
 
+# function to decode bencoded strings.
 def decode(data):
+	# check to see if the data is an integer.
 	if BENCODED_INTEGER_RE.match(data):
+		# remove the start and end delimeters.
 		number_string = data[1:-1]
 
 		# check no padding zeros.
 		if len(number_string) > 1 and number_string[0] == "0":
 			raise DecodeError("Padding zeros is not allowed.")
 
+		# return an integer.
 		return int(number_string)
