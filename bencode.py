a527a2ba0afa14776fdb4655523e76a3091c5950
Joseph Salisbury <salisbury.joseph@gmail.com>
2012-06-11 17:27:23 +0100

    Add: git ignore file, to ignore .pyc files. Add: test that we can decode i3e. Add: function to decode, or stub at least.

---
+++ bencode.py
@@ -0,0 +1,5 @@
+# bencode.py -- module for dealing with bencoded data.
+# written by Joseph Salisbury -- 11/06/12
+
+def decode(data):
+	return 3
---
+++ decode_tests.py
@@ -0,0 +1,7 @@
+# decode_tests.py -- tests for decoding bencoded data.
+# written by Joseph Salisbury -- 11/06/12
+
+from bencode import decode
+
+def test_decode_int():
+	assert decode("i3e") == 3
