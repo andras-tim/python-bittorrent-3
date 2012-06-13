159aec91041e7070851eab55ccb22398c8c753da
Joseph Salisbury <salisbury.joseph@gmail.com>
2012-06-13 18:11:56 +0100

    Refactor: decode_tests split into tests for strings and integers.

--- decode_integer_tests.py
+++ decode_integer_tests.py
@@ -1,25 +1,6 @@
-# decode_tests.py -- tests for decoding bencoded data.
-# written by Joseph Salisbury -- 11/06/12
+# decode_integer_tests.py -- tests for decoding bencoded integers.
 
-from bencode import decode, DecodeError
-
-# test that we can decode the empty string
-def test_decode_empty_string():
-	assert decode("0:") == ""
-
-# test that we can decode a simple string.
-def test_decode_simple_string():
-	assert decode("4:spam") == "spam"
-
-# test that we can decode a longer string.
-def test_decode_long_string():
-	LONG_STRING = "antidisestablishmentarianism"
-
-	assert decode("28:" + LONG_STRING) == LONG_STRING
-
-# test that we can decode a string with capital letters.
-def test_decode_capital_letters():
-	assert decode("13:camelCaseTest") == "camelCaseTest"
+from bencode import decode
 
 # test that we can decode an integer
 def test_decode_int():
---
+++ decode_string_tests.py
@@ -0,0 +1,21 @@
+# decode_tests.py -- tests for decoding bencoded data.
+
+from bencode import decode
+
+# test that we can decode the empty string
+def test_decode_empty_string():
+	assert decode("0:") == ""
+
+# test that we can decode a simple string.
+def test_decode_simple_string():
+	assert decode("4:spam") == "spam"
+
+# test that we can decode a longer string.
+def test_decode_long_string():
+	LONG_STRING = "antidisestablishmentarianism"
+
+	assert decode("28:" + LONG_STRING) == LONG_STRING
+
+# test that we can decode a string with capital letters.
+def test_decode_capital_letters():
+	assert decode("13:camelCaseTest") == "camelCaseTest"
