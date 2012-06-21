0b517ed2ccae4208f5a1de0b008fd2458233696c
Joseph Salisbury <salisbury.joseph@gmail.com>
2012-06-21 23:16:07 +0100

    Fix: Change to reflect that map() returns an iterator in py3.

--- bencode.py
+++ bencode.py
@@ -36,7 +36,7 @@ def decode(data):
 			# recursively decode the data,
 			# after removing the list delimeters,
 			# and add the returning data to the list.
-			return map(decode, split(data[1:-1]))
+			return list(map(decode, split(data[1:-1])))
 
 # function to split a bencoded string into its components.
 def split(data):
