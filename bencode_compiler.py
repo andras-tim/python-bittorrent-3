4712bbbf2729afc4d517c8e889188f44e526fb85
Joseph Salisbury <salisbury.joseph@gmail.com>
2012-07-16 00:20:01 +0100

    Add: test we can parse an integer token into a parse tree. Add: little class for the parse tree nodes. Add: parse function to parse an integer into a node.

--- bencode_compiler.py
+++ bencode_compiler.py
@@ -1,5 +1,15 @@
 # compiler.py -- bencode compiler
 
+# a node for the bencode parse tree
+class node():
+	# initialise the node with the token data.
+	def __init__(self, data):
+		self.data = data
+
+	# two nodes are equal if they contain the same token.
+	def __eq__(self, other):
+		return self.data == other.data
+
 # turn a string of bencoded data into a tokenised list.
 def tokenise(data):
 	# if the data is a bencoded integer.
@@ -7,3 +17,10 @@ def tokenise(data):
 		# find the end, and splice it out.
 		end_index = data.find("e")
 		return [data[:end_index + 1]]
+
+# turn a list of tokens into a parse tree.
+def parse(tokens):
+	# if the first token is an integer,
+	if tokens[0].startswith("i"):
+		# then just make a node out of the token.
+		return node(tokens[0])
--- compiler_tests.py
+++ compiler_tests.py
@@ -1,7 +1,12 @@
 # compiler_tests.py -- tests for the bencode compiler_tests
 
-from bencode_compiler import tokenise
+from bencode_compiler import tokenise, parse, node
 
 # test we can tokenise a bencoded integer.
 def test_tokenise_integer():
 	assert tokenise("i0e") == ["i0e"]
+
+# test we can parse an integer into a tree (albeit with one node).
+def test_parse_integer():
+	integer_node = node("i0e")
+	assert parse(["i0e"]) == integer_node
